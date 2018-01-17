8b623ab90ae1d958dab32fd293c1b14534c112ed
taomingwang <taojike@126.com>
2018-01-17 13:48:40 +0800

    开始编写云函数

--- cloud.js
+++ cloud.js
@@ -6,3 +6,25 @@ var AV = require('leanengine');
 AV.Cloud.define('hello', function(request) {
   return 'Hello world!';
 });
+
+var Document = AV.Object.extend('document');
+
+// 查询 document 列表
+AV.Cloud.define('hello', function(request) {
+    var query = new AV.Query(Document);
+    query.equalTo('state',"1");
+    query.find().then(function(results) {
+
+
+
+
+    }, function(err) {
+        if (err.code === 101) {
+            // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
+            // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
+
+        } else {
+            next(err);
+        }
+    }).catch(next);
+});
